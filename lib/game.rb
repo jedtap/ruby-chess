cdbe1b13a966c9b80a310a469f8c7ab97654c88b
Jed-Tapiador <doom.slaver2@gmail.com>
2022-02-19 18:27:19 +0800

    add introduction

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,35 @@
 # frozen_string_literal: true
 
+require_relative "symbols"
+
 class Game
   def play
-    puts "Why hello there"    
+    introduction 
   end
-end
+
+  private
+
+  def introduction   
+    puts <<~HEREDOC
+
+    Welcome to Chess board game on Ruby terminal!
+    Checkmate the opponent's king and win!
+    Goodluck and have fun!
+
+    Enter..
+    [1] To start a player vs player game
+    [2] To start a player vs computer game
+    [3] To resume from saved game
+
+    HEREDOC
+    loop do     
+      print "Input: "
+      input = gets[0].to_i
+      break if input.between?(1,1)
+
+      puts "Invalid input.. Try again."
+    end
+  end 
+
+
+end # End of Game class!
