d357a6d1adc749f0860dc0aa00ce148936416bf9
Jed-Tapiador <doom.slaver2@gmail.com>
2022-02-19 21:04:41 +0800

    reversed print order and highlight any coordinate to green

--- lib/board.rb
+++ lib/board.rb
@@ -7,43 +7,37 @@ class Board
 
   def initialize
     @board = [[],[],["#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}"],["#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}"], ["#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}"], ["#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}","#{empty}"],[],[]]
-    @board[0] = ["#{b_rook}","#{b_knight}","#{b_bishop}","#{b_queen}","#{b_king}","#{b_bishop}","#{b_knight}","#{b_rook}"]
-    @board[1] = ["#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}"]
-    @board[6] = ["#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}"]
-    @board[7] = ["#{w_rook}","#{w_knight}","#{w_bishop}","#{w_queen}","#{w_king}","#{w_bishop}","#{w_knight}","#{w_rook}"]
+    @board[0] = ["#{w_rook}","#{w_knight}","#{w_bishop}","#{w_queen}","#{w_king}","#{w_bishop}","#{w_knight}","#{w_rook}"]
+    @board[1] = ["#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}","#{w_pawn}"]
+    @board[6] = ["#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}","#{b_pawn}"]
+    @board[7] = ["#{b_rook}","#{b_knight}","#{b_bishop}","#{b_queen}","#{b_king}","#{b_bishop}","#{b_knight}","#{b_rook}"]
+    dark = 44
+    light = 1
+    @color = [[dark,light,dark,light,dark,light,dark,light], [light,dark,light,dark,light,dark,light,dark], [dark,light,dark,light,dark,light,dark,light], [light,dark,light,dark,light,dark,light,dark],[dark,light,dark,light,dark,light,dark,light], [light,dark,light,dark,light,dark,light,dark], [dark,light,dark,light,dark,light,dark,light], [light,dark,light,dark,light,dark,light,dark]]
   end
 
   def print_board
+    Gem.win_platform? ? (system "cls") : (system "clear")
     row_num = 8
     puts "    a  b  c  d  e  f  g  h"
-      @board.each do |row|
-        print " #{row_num} "
-        row.each do |cell|
-          print cell
-        end
-        print " #{row_num} "
-        row_num -= 1
-        print "\n"
+    for row in (7).downto(0)
+      print " #{row_num} "
+      for column in 0..7
+        print "\e[#{@color[row][column]}m#{@board[row][column]}\e[0m"
       end
-    puts "    a  b  c  d  e  f  g  h"
+      print " #{row_num} "
+      row_num -= 1
+      print "\n"
+    end
+    puts "    a  b  c  d  e  f  g  h\n\n"
   end
 
-  def test_items
-    puts "white:"
-    puts "\e[30;44m \u2b24  \e[0m"
-    puts "\e[30;44m \u2654  \e[0m"
+  def color_move(coordinates)
+    row = coordinates[0]
+    column = coordinates[1]
+    @color[row][column] = 42
   end
-end
+
+end # End of board class!
 
 #bg red = 41
-
-#black and white pieces 30 and 0
-
-#bg blue = 44
-#bg gray = 47
-
-#8
-#down
-#1
-#a right h
-
--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,31 @@ require_relative "board"
 class Game
   def initialize
     @board = Board.new
+    @current = "White"
+    @coordinates = []
   end
 
   def play
     introduction
     @board.print_board
+    @coordinates = select_piece
+    @board.color_move(@coordinates)
+    @board.print_board
+  end
+
+  def select_piece
+    puts "#{@current}'s Turn!\n"
+    puts "Enter coordinates of the piece you want to move,"
+    puts "enter [1] to save, or [0] to quit."
+    loop do
+      print "Input: "
+      @coordinates = gets
+      row = @coordinates[1].to_i - 1
+      column = @coordinates[0].to_s.downcase.ord - 97
+      return [row, column] if row.between?(0,7) && column.between?(0,7)
+
+      puts "Invalid input.. Try again"
+    end
   end
 
   private
