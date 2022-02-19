c4f6b690f8c10da316f7ad069c9c11d21e0019ba
Jed-Tapiador <doom.slaver2@gmail.com>
2022-02-19 18:17:13 +0800

    added chess pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,22 @@
 class Board
+  puts "white:"
+  puts "\e[30;44m \u2b24  \e[0m"
+  puts "\e[30;44m \u2654  \e[0m"
 
 end
+
+=begin
+
+bg red = 41
+
+black and white pieces 30 and 0
+
+bg blue = 44
+bg gray = 47
+
+8
+down
+1
+a right h
+
+  =end
--- lib/symbols.rb
+++ lib/symbols.rb
@@ -1,9 +1,61 @@
 # frozen_string_literal: true
 
 module Symbols
-  def dummy_ascii
+  def test_item
     "\e[31m\u25cf\e[0m"
     "\u25cb"
+  end
+
+  def w_king
+    "\u2654"
+  end
+
+  def w_king
+    "\u2654"
+  end
+
+  def w_queen
+    "\u2655"
+  end
+
+  def w_rook
+    "\u2656"
+  end
+
+  def w_bishop
+    "\u2657"
+  end 
+
+  def w_knight
+    "\u2658"
+  end
+
+  def w_pawn
+    "\u2659"
+  end
+
+  def b_king
+    "\u265A"
+  end
+
+  def b_queen
+    "\u265B"
+  end
+  
+  def b_rook
+    "\u265C"
+  end
+
+  def b_bishop
+    "\u265D"
+  end 
+
+  def b_knight
+    "\u265E"
+  end
+
+  def b_pawn
+    "\u265F"
   end
 
 end
